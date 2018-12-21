4db32fb36d9cd346d8cb2e11d1a72e0472aa947e
Benjamin <benjamin10882579@yahoo.com>
2018-12-21 00:03:53 -0800

    Custom command for running docker was added

--- apps/api/management/commands/rundocker.py
+++ apps/api/management/commands/rundocker.py
@@ -5,8 +5,5 @@ import os
 class Command(BaseCommand):
     help = 'Runs docker server'
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('poll_id', nargs='+', type=int)
-
     def handle(self, *args, **options):
         os.system('docker run -p 6379:6379 -d redis:2.8')
